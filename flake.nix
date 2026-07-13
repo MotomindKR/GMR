142740d20f90efc7c5e1c13f3b6ad81d78fd3e30
Gyungmin Myung <gmmyung@motomind.co.kr>
2026-07-13 15:38:07 +0900

    Add reproducible Nix retargeting environment

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Reproducible GMR development and retargeting environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        runtimeLibraries = with pkgs; [
+          libGL
+          libglvnd
+          glfw
+          stdenv.cc.cc.lib
+          libx11
+          libxcursor
+          libxext
+          libxi
+          libxinerama
+          libxrandr
+          libxcb
+          zlib
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            ffmpeg
+            git
+            python311
+            uv
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibraries;
+          UV_CACHE_DIR = ".uv-cache";
+          UV_PROJECT_ENVIRONMENT = ".venv";
+          UV_PYTHON_DOWNLOADS = "never";
+
+          shellHook = ''
+            set -e
+
+            lock_hash="$(sha256sum requirements.lock | cut -d' ' -f1)"
+            stamp="$UV_PROJECT_ENVIRONMENT/.gmr-requirements-lock"
+
+            if [ ! -x "$UV_PROJECT_ENVIRONMENT/bin/python" ] || [ "$(cat "$stamp" 2>/dev/null)" != "$lock_hash" ]; then
+              uv venv --python ${pkgs.python311}/bin/python --clear
+              uv pip sync --torch-backend cpu requirements.lock
+              printf '%s\n' "$lock_hash" > "$stamp"
+            fi
+
+            source "$UV_PROJECT_ENVIRONMENT/bin/activate"
+
+            if [ -z "''${MUJOCO_GL:-}" ]; then
+              if [ -n "''${DISPLAY:-}" ]; then
+                export MUJOCO_GL=glfw
+              else
+                export MUJOCO_GL=egl
+              fi
+            fi
+          '';
+        };
+      }
+    );
+}
